530a4497b834de49c9a27ccd93a942f9cee75599
satvik <jasatvik@gmail.com>
2020-11-09 05:38:34 +0530

    initial commit

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,58 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+//inside any middleware function you get req,res and next as arguments
+const requireAuth = (req,res,next) => {
+    const token = req.cookies.jwt;
+
+    //check if jwt exists and is verified
+    if(token) {
+        jwt.verify(token, 'sseeccrreett', (err, decodedToken)=>{
+            if(err){
+                console.log(err.message);
+                res.redirect('/login');
+            }
+            else{
+                console.log(decodedToken)
+                next();
+            }
+
+        });
+    }
+    else{
+        res.redirect('/login');
+
+    }
+
+    
+}
+//check current user
+
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if(token){
+        jwt.verify(token, 'sseeccrreett', async(err, decodedToken)=>{
+            if(err){
+                console.log(err.message);
+                res.locals.user = null; 
+                next();
+            }
+            else{
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+
+        });
+
+
+    }
+    else{
+        res.locals.user = null;
+        next();
+
+    }
+}
+module.exports = { requireAuth, checkUser };
